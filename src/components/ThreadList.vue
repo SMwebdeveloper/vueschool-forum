9ff0e49fddf5723b695104cbb0b5cec307afacca
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-12 17:54:16 +0500

    ThreadList and ThreadListItem components

---
+++ src/components/ThreadList.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="thread-list">
+        <h2 class="list-title">Threads</h2>
+        <ThreadListItem 
+          v-for="thread in threads"
+          :thread="thread"
+          :key="thread['.key']"
+        />
+    </div>
+</template>
+<script>
+import ThreadListItem from './ThreadListItem.vue'
+export default {
+  components: {
+    ThreadListItem
+  },
+  props: {
+    threads: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
