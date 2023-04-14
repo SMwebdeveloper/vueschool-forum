d1db0855391358cf84b70b3f86ad0dcacd74c584
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-14 11:41:07 +0500

    ForumList and ForumListItem component

---
+++ src/components/ForumList.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="forum-list">
+      <h2 class="list-title">
+        <a href="category.html">Feedback &amp; Infomation</a>
+      </h2>
+      <ForumListItem
+        v-for="forum in forums"
+        :forum="forum"
+        :key="forum['.key']"/>
+    </div>
+</template>
+<script>
+import ForumListItem from './ForumListItem.vue'
+export default {
+  components: {
+    ForumListItem
+  },
+  props: {
+    forums: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
+<style lang="">
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="col-full">
       <h1>Welcome to the Forum</h1>
-      <ThreadList :threads="threads"/>
+      <ForumList :forums="forums"/>
     </div>
   </template>
 
   <script>
   import sourceData from '@/data'
-  import ThreadList from '@/components/ThreadList.vue'
+  import ForumList from '@/components/ForumList.vue'
   console.log(sourceData)
   export default {
     name: 'HelloWorld',
     components: {
-      ThreadList
+      ForumList
     },
     data () {
       return {
-        threads: Object.values(sourceData.threads),
+        forums: Object.values(sourceData.forums),
         posts: sourceData.posts,
         users: sourceData.users
       }
