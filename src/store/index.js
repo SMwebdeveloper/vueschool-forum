167f3803bd013e2f1ebad089b17e412495127607
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-20 16:50:52 +0500

    create fetchItems and fetchPosts actions

--- src/store/index.js
+++ src/store/index.js
@@ -108,6 +108,9 @@ export default new Vuex.Store({
     fetchPost ({dispatch}, {id}) {
       return dispatch('fetchItem', {resource: 'posts', id, emoji: '🎈'})
     },
+    fetchPosts ({dispatch}, {ids}) {
+      return dispatch('fetchItems', {resource: 'posts', emoji: 'chat', ids})
+    },
     fetchItem ({commit, state}, {id, emoji, resource}) {
       console.log('osha', emoji, id)
       return new Promise((resolve, reject) => {
@@ -117,6 +120,9 @@ export default new Vuex.Store({
           resolve(state[resource][id])
         })
       })
+    },
+    fetchItems ({dispatch}, {ids, resource, emoji}) {
+      return Promise.all(ids.map(id => dispatch('fetchItem', {id, resource, emoji})))
     }
   },
   mutations: {
