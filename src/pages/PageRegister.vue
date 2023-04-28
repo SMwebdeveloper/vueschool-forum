4f17bfa3a86c2cd20f084554ac5754cf03a40822
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-04-28 17:14:56 +0500

    sign in with google

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -54,7 +54,7 @@
         </div>
       </form>
       <div class="text-center push-top">
-        <button class="btn-red btn-xsmall">
+        <button @click="registerWithGoogle" class="btn-red btn-xsmall">
           <i class="fa fa-google fa-btn"></i>Sign up with Google
         </button>
       </div>
@@ -78,6 +78,12 @@ export default {
   methods: {
     register () {
       this.$store.dispatch('registerUserWithEmailAndPassword', this.form)
+        .then(() => {
+          this.$router.push('/')
+        })
+    },
+    registerWithGoogle () {
+      this.$store.dispatch('signInWithGoogle')
         .then(() => {
           this.$router.push('/')
         })
--- src/pages/PageSignin.vue
+++ src/pages/PageSignin.vue
@@ -35,7 +35,7 @@
       </form>
 
       <div class="push-top text-center">
-        <button class="btn-red btn-xsmall">
+        <button @click="signInWithGoogle" class="btn-red btn-xsmall">
           <i class="fa fa-google fa-btn"></i>Sign in with Google
         </button>
       </div>
@@ -60,6 +60,11 @@ export default {
           email: this.form.email,
           password: this.form.password
         })
+        .then(() => this.$router.push('/'))
+        .catch(error => alert('🤷‍️' + error.message))
+    },
+    signInWithGoogle () {
+      this.$store.dispatch('signInWithGoogle')
         .then(() => this.$router.push('/'))
         .catch(error => alert('🤷‍️' + error.message))
     }
