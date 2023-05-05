ca435a2c3d188fc6ba267060ff2d1c7161caf3c4
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-05 11:14:24 +0500

    validators signin

--- src/components/UserProfileCardEditor.vue
+++ src/components/UserProfileCardEditor.vue
@@ -7,20 +7,25 @@
 
       <div class="form-group">
         <input
-          v-model="activeUser.username"
-          type="text"
-          placeholder="Username"
-          class="form-input text-lead text-bold"
-        />
+          v-model.lazy="activeUser.username"
+          @blur="$v.activeUser.username.$touch()"
+          type="text" placeholder="Username" class="form-input text-lead text-bold"
+        >
+        <template v-if="$v.activeUser.username.$error">
+          <span v-if="!$v.activeUser.username.required" class="form-error">This field is required</span>
+          <span v-if="!$v.activeUser.username.unique" class="form-error">Sorry! This username is taken</span>
+        </template>
       </div>
 
       <div class="form-group">
         <input
           v-model="activeUser.name"
-          type="text"
-          placeholder="Full Name"
-          class="form-input text-lead"
-        />
+          @blur="$v.activeUser.name.$touch()"
+          type="text" placeholder="Full Name" class="form-input text-lead"
+        >
+        <template v-if="$v.activeUser.name.$error">
+          <span v-if="!$v.activeUser.name.required" class="form-error">The name field is required</span>
+        </template>
       </div>
 
       <div class="form-group">
@@ -53,11 +58,15 @@
       <div class="form-group">
         <label class="form-label" for="user_email">Email</label>
         <input
-          v-model="activeUser.email"
-          autocomplete="off"
-          class="form-input"
-          id="user_email"
-        />
+          v-model.lazy="activeUser.email"
+          @blur="$v.activeUser.email.$touch()"
+          autocomplete="off" class="form-input" id="user_email"
+        >
+        <template v-if="$v.activeUser.email.$error">
+          <span v-if="!$v.activeUser.email.required" class="form-error">This field is required</span>
+          <span v-else-if="!$v.activeUser.email.email" class="form-error">This in not a valid email address</span>
+          <span v-else-if="!$v.activeUser.email.unique" class="form-error">Sorry! This email is taken</span>
+        </template>
       </div>
 
       <div class="form-group">
@@ -85,6 +94,8 @@
 </template>
 
 <script>
+import { required, email } from 'vuelidate/lib/validators'
+import { uniqueUsername, uniqueEmail } from '@/utils/validators'
 export default {
   props: {
     user: {
@@ -103,6 +114,32 @@ export default {
     },
     userPostsCount () {
       return this.$store.getters['users/userPostsCount'](this.user['.key'])
+    }
+  },
+  validations: {
+    activeUser: {
+      name: {
+        required
+      },
+      username: {
+        required,
+        unique (value) {
+          if (value.toLowerCase() === this.user.usernameLower) {
+            return true
+          }
+          return uniqueUsername(value)
+        }
+      },
+      email: {
+        required,
+        email,
+        unique (value) {
+          if (value.toLowerCase() === this.user.email) {
+            return true
+          }
+          return uniqueEmail(value)
+        }
+      }
     }
   },
   methods: {
