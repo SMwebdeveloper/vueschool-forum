806eb4cf3629ba3772e6260999abcec2cdd1caf1
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-05-05 10:54:33 +0500

    custom validators

--- src/pages/PageRegister.vue
+++ src/pages/PageRegister.vue
@@ -107,8 +107,8 @@
 </template>
 
 <script>
-import firebase from 'firebase'
-import { required, email, minLength, url, helpers as vuelidateHelpers } from 'vuelidate/lib/validators'
+import {required, email, minLength, url} from 'vuelidate/lib/validators'
+import {uniqueEmail, uniqueUsername, responseOk, supportedImageFile} from '@/utils/validators'
 export default {
   data () {
     return {
@@ -128,28 +128,12 @@ export default {
       },
       username: {
         required,
-        unique (value) {
-          if (!vuelidateHelpers.req('value')) {
-            return true
-          }
-          return new Promise((resolve, reject) => {
-            firebase.database().ref('users').orderByChild('usernameLower').equalTo(value.toLowerCase())
-              .once('value', snapshot => resolve(!snapshot.exists()))
-          })
-        }
+        unique: uniqueUsername
       },
       email: {
         required,
         email,
-        unique (value) {
-          if (!vuelidateHelpers.req('value')) {
-            return true
-          }
-          return new Promise((resolve, reject) => {
-            firebase.database().ref('users').orderByChild('email').equalTo(value.toLowerCase())
-              .once('value', snapshot => resolve(!snapshot.exists()))
-          })
-        }
+        unique: uniqueEmail
       },
       password: {
         required,
@@ -157,24 +141,8 @@ export default {
       },
       avatar: {
         url,
-        supportedImageFile (value) {
-          if(!vuelidateHelpers.req(value)) {
-            return true
-          }
-          const supported = ['jpg', 'jpeg', 'gif', 'png', 'svg']
-          const suffix = value.split('.').pop()
-          return supported.includes(suffix)
-        },
-        responseOk (value) {
-          if(!vuelidateHelpers.req(value)) {
-            return true
-          }
-          return new Promise((resolve, reject) => {
-            fetch(value)
-              .then(response => resolve(response.ok))
-              .catch(() => resolve(false))
-          })
-        }
+        supportedImageFile,
+        responseOk
       }
     }
   },
